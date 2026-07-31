ab295e1f6b109134a0bfbf7fe62f031a814581dd
Camilo Henriquez <130488549+hnkatze@users.noreply.github.com>
2026-07-31 09:55:26 -0600

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Hector  on 31/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
